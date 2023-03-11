c5ee7ccba74500d2b45eb80fb3e0ac0d26b84bb0
Shayaan Ahmed <117999566+shayaan-ahm3d@users.noreply.github.com>
2023-03-11 23:38:27 +0000

    BROKEN: unfinished MyGameState

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -4,10 +4,13 @@ import com.google.common.collect.ImmutableList;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
 
 import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -22,10 +25,10 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			ImmutableList<Player> detectives) {
 		// TODO
 		/* Test 1*/
-		if(mrX == null)
+		if (mrX == null)
 			throw new NullPointerException();
 		/* Test 4 */
-		else if(mrX.isDetective()== true)
+		else if (mrX.isDetective())
 			throw new IllegalArgumentException();
 
 		/* Test 7 - Create Set for colors and locations and throw if a repeat is added*/
@@ -36,17 +39,17 @@ public final class MyGameStateFactory implements Factory<GameState> {
 		/* Test 2 + 3*/
 		for (int i = 0; i < detectives.size(); i++) {
 			/* Test 7 Implementation */
-			if (colorSet.add(detectives.get(i).piece().webColour()) == false)
+			if (!colorSet.add(detectives.get(i).piece().webColour()))
 				throw new IllegalArgumentException();
 			/* Test 8*/
-			else if (startLocationSet.add(detectives.get(i).location()) == false)
+			else if (!startLocationSet.add(detectives.get(i).location()))
 				throw new IllegalArgumentException();
 			/* Test 8 END */
 			/* Test 7 END*/
 			if (detectives.get(i) == null)
 				throw new NullPointerException();
 				/* Test 5*/
-			else if (detectives.get(i).isDetective() == false)
+			else if (!detectives.get(i).isDetective())
 				throw new IllegalArgumentException();
 			/* Test 6 passes prior to this with no implementation*/
 			/* Test 9 + 10 */
@@ -54,10 +57,93 @@ public final class MyGameStateFactory implements Factory<GameState> {
 				throw new IllegalArgumentException();
 			/* Test 9 + 10 End*/
 		}
-
-
-		throw new RuntimeException("Implement me!");
-
+		return new MyGameState(setup,
+				ImmutableSet.of(Piece.MrX.MRX),
+				ImmutableSet.of(), // What goes here? They don't say
+				mrX,
+				detectives);
 	}
+	private final class MyGameState implements GameState {
+		private GameSetup setup;
+		private ImmutableSet<Piece> remaining;
+		private ImmutableList<LogEntry> log;
+		private Player mrX;
+		private List<Player> detectives;
+		private ImmutableSet<Move> moves;
+		private ImmutableSet<Piece> winner;
 
+		private MyGameState(final GameSetup setup,
+							final ImmutableSet<Piece> remaining,
+							final ImmutableList<LogEntry> log,
+							final Player mrX,
+							final List<Player> detectives) {
+			this.setup = setup;
+			this.remaining = remaining;
+			this.log = log;
+			this.mrX = mrX;
+			this.detectives = detectives;
+
+			if (setup == null) {
+				throw new NullPointerException("Setup is null");
+			} else if (remaining == null) {
+				throw new NullPointerException("Remaining is null");
+			} else if (log == null) {
+				throw new NullPointerException("Log is null");
+			} else if (mrX == null) {
+				throw new NullPointerException("Mr X is null");
+			} else if (detectives == null) {
+				throw new NullPointerException("Detectives is null");
+			} else if (setup.moves.isEmpty()) {
+				throw new IllegalArgumentException("Moves is empty!");
+			}
+		}
+
+		@Nonnull
+		@Override
+		public GameSetup getSetup() {
+			return setup;
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableSet<Piece> getPlayers() {
+			return null;
+		}
+
+		@Nonnull
+		@Override
+		public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
+			return Optional.empty();
+		}
+
+		@Nonnull
+		@Override
+		public Optional<TicketBoard> getPlayerTickets(Piece piece) {
+			return Optional.empty();
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableList<LogEntry> getMrXTravelLog() {
+			return getMrXTravelLog();
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableSet<Piece> getWinner() {
+			return null;
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableSet<Move> getAvailableMoves() {
+			return null;
+		}
+
+		@Nonnull
+		@Override
+		public GameState advance(Move move) {
+			return null;
+		}
+	}
 }
